1db6218f6773df1a69432732012ce1944b601db6
kemonicodes <kemoniw333@gmail.com>
2024-03-14 00:36:09 -0700

    clean up

---
+++ app/components/Home/hero.tsx
@@ -0,0 +1,9 @@
+import React, {useEffect} from "react"
+
+export default function Hero() {
+    return (
+        <>
+        </>
+
+    )
+}
---
+++ app/components/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from './hero'
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
