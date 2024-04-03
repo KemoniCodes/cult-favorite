727e9721fef823275514afc0632d03b4014e0e9c
kemonicodes <kemoniw333@gmail.com>
2024-04-03 01:19:27 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import SmoothScrolling from "./utils/smoothScrolling";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 // import Nav from "./components/Layout/nav";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* <Nav/> */}
         <SpeedInsights/>
+        <Analytics/>
         <SmoothScrolling>
           {children}
         </SmoothScrolling>
