9d67234dda80f7510c360c4b0592f92ac7763138
kemonicodes <kemoniw333@gmail.com>
2024-04-07 00:06:38 -0700

    tweaks

--- app/utils/smoothScrolling.jsx
+++ app/utils/smoothScrolling.jsx
@@ -3,7 +3,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true, smooth: true }}>
       {children}
     </ReactLenis>
   );
