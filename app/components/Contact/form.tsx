864d0e06c09c589a7dd1fd3387218dd047a10fea
kemonicodes <kemoniw333@gmail.com>
2024-04-13 00:06:44 -0700

    client inquiry form

---
+++ app/components/Contact/form.tsx
@@ -0,0 +1,89 @@
+import React, { useRef, useState } from "react";
+
+export default function Form() {
+    const formRef = useRef<HTMLFormElement>(null);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const requestOptions = {
+            method: 'POST',
+            body: formData,
+            mode: 'no-cors' as RequestMode
+        };
+        try {
+            const response = await fetch(form.action, requestOptions);
+            if (response.ok) {
+                setFormSubmitted(true);
+            } else {
+                setFormSubmitted(false);
+            }
+        } catch (error) {
+
+        }
+    };
+
+    const handleButtonClick = () => {
+        formRef?.current?.classList.add('hidden');
+        setFormSubmitted(true);
+        setTimeout(() => {
+            setFormSubmitted(false);
+            formRef?.current?.classList.remove('hidden');
+            formRef?.current?.reset();
+        }, 3000);
+    };
+    return (
+        <>
+            {formSubmitted && <h3 className="successMessage pt-16">Form submitted successfully!</h3>}
+
+            <form action="https://postend.vercel.app/n/gAAAAABmGeveAd_SEHJf_e6lF-H6NhdT5yRsDnlebfku-7P8nJkFUkOqvsKH8Oh7VO5Ec0w7wHeI-bfzUYdnD9sI3DHUkOLZUx4Ug8oLrVcdiToz2FHuizBuEzduJO-1NporZEIZRbzituf7YB7xO8ISr_ehEf6qLVq2b4RnZYvJWhNuqSqdUxkt_9ayyNKZItblGcMLEUbx" method="POST" onSubmit={handleSubmit} ref={formRef} className="w-[80%] m-auto mt-24">
+
+                <div className="lg:flex block gap-24 justify-between lg:pb-8 pb-0">
+                    <div className="flex flex-col w-full lg:pb-0 pb-[2.5rem]">
+                        <label htmlFor='first_name' className="h3 text-left pb-2">first name</label>
+                        <input type="text" name="first_name" />
+                    </div>
+                    <div className="flex flex-col w-full lg:pb-0 pb-[2.5rem]">
+                        <label htmlFor='email' className="h3 text-left pb-2">email</label>
+                        <input type="email" name="email" />
+                    </div>
+                </div>
+                <div className="lg:flex block gap-24 justify-between pb-8">
+                    <div className="flex flex-col w-full lg:pb-0 pb-[2.5rem]">
+                        <label htmlFor='business_name' className="h3 text-left pb-2">business name</label>
+                        <input type="text" name="business_name" />
+                    </div>
+                    <div className="flex flex-col w-full lg:pb-0 pb-[2.5rem]">
+                        <label htmlFor='budget' className="h3 text-left pb-2">budget</label>
+                        <select name="budget" id="budget" className="h3">
+                            <option value="<10K">&lt;10k</option>
+                            <option value=">10K">&gt;10k</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="lg:flex block flex-col pb-16 lg:w-1/2 w-full">
+                    <label htmlFor='findUs' className="h3 text-left pb-2 lg:float-none float-left">how'd you hear about the studio?</label>
+                    <select name="findUs" id="findUs" className="h3 lg:w-auto w-full lg:mt-0 mt-4">
+                        <option value="instagram">instagram</option>
+                        <option value="refferal">referral</option>
+                        <option value="behance">behance</option>
+                        <option value="linkedin">linkedin</option>
+                        <option value="google">google</option>
+                    </select>
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor='services' className="h3 text-left pb-2">what do you need help with?</label>
+                    <textarea name="services" className="h3"></textarea>
+                </div>
+                <div className="flex">
+                    <button onClick={handleButtonClick} type="submit" className="h3 hover:!text-offBlack bg-transparent border-[1.5px] border-blancheWhite text-blancheWhite py-[.8rem] px-8 button hover:bg-tarantinoYellow hover:border-tarantinoYellow hover:rounded-[.8rem] mt-8 duration-200 ease-in-out">Submit</button>
+                </div>
+            </form>
+        </>
+    )
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,21 +1,22 @@
-'use client'
-import React, { useEffect, useMemo, useState } from "react";
-import { usePathname } from "next/navigation";
+"use client"
+import React, { useEffect, useMemo, useState, useRef } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import Nav from "../components/Layout/nav";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import Footer from "../components/Layout/footer";
+import Form from "../components/Contact/form";
 
 export default function ContactPage() {
     const [date, setDate] = useState('');
     const [currentTime, setCurrentTime] = useState('');
-    const router = usePathname();
+    const currentLink = usePathname();
 
     useEffect(() => {
-        if (router) {
+        if (currentLink) {
             document.querySelector('.contactLink')?.classList.add('linkActive')
         }
-    }, [router])
+    }, [currentLink]);
 
     const formattedDate = useMemo(() => {
         const today = new Date();
@@ -42,7 +43,8 @@ export default function ContactPage() {
         }, 1000);
 
         return () => clearInterval(interval);
-    }, [])
+    }, []);
+
     return (
         <>
             <div className="contactPageContainer pb-16 lg:w-max w-full m-auto">
@@ -51,7 +53,6 @@ export default function ContactPage() {
                 </div>
                 <motion.h1 className="lg:text-[200px] 
                 text-[130px] uppercase lg:leading-[12rem] leading-[7.5rem] text-center mt-[11rem] lg:mb-4 mb-8" initial={{ opacity: 0 }}
-                    // style={{ y: 1400 }} 
                     transition={{
                         ease: "linear",
                         duration: .3
@@ -61,7 +62,6 @@ export default function ContactPage() {
                 </motion.h1>
                 <motion.div className="about pt-2 relative text-center "
                     initial={{ opacity: 0 }}
-                    // style={{ y: 1400 }} 
                     transition={{
                         ease: "linear",
                         duration: .3
@@ -71,11 +71,12 @@ export default function ContactPage() {
                     <h3 className="lg:mb-0 mb-2">GOT A PROJECT? GIVE ME A RING(EMAIL THE STUDIO)</h3>
                     <h3 className="lg:mb-0 mb-2">ANOTHER CREATIVE WANTING TO COLLAB? LET’S DO IT, IMMEDIATELY</h3>
                     <h3 className="lg:mb-0 mb-2">HAVE SOME KIND WORDS? PLZ SEND</h3>
+                    <Form />
                     <Link href={'mailto:HELLO@CULT-FAVORITE.COM'}>
-                        <h2 className=" my-16">HELLO@CULT-FAVORITE.COM</h2>
+                        <h2 className=" mt-24 mb-16">HELLO@CULT-FAVORITE.COM</h2>
                     </Link>
 
-                    <div className="info flex justify-center gap-16">
+                    <div className="info flex justify-around gap-16">
                         <ul className="text-left">
                             <li className="h3">{date}</li>
                             <li className="h3">{currentTime}</li>
@@ -95,7 +96,7 @@ export default function ContactPage() {
                     </div>
                 </motion.div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
